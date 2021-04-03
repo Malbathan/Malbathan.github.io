a716ca6484768be7fe0d11b4c0806f978633e625
Malbathan Lima <malbathanlima@gmail.com>
2021-04-03 19:00:32 -0300

    Todo list modifications

--- portfolio/TodoList/script.js
+++ portfolio/TodoList/script.js
@@ -61,3 +61,82 @@ fChild.insertBefore(bt1, listOrder);
 const img = document.createElement('img');
 img.setAttribute ('src', 'img/prancheta-com-ilustracao-vetorial-de-temporizador_138676-255-removebg-preview.png');
 fChild.append(img);
+
+const buttons = document.createElement('section');
+buttons.classList = "buttons-header";
+// const bt2 = document.createElement('button');
+// bt2.id = 'mover-cima';
+// bt2.innerHTML = 'Mover(up)';
+// const bt3 = document.createElement('button');
+// bt3.id = 'mover-baixo';
+// bt3.innerHTML = 'Mover(down)';
+const bt4 = document.createElement('button');
+bt4.id = 'remover-selecionado';
+bt4.innerHTML = 'Remove(selected)';
+
+const bt5 = document.createElement('button');
+bt5.id = 'remover-completo';
+bt5.innerHTML = 'Remove(completed)';
+
+const bt6 = document.createElement('button');
+bt6.id = 'Remove(all)';
+bt6.innerHTML = 'Remove(all)';
+
+const bt7 = document.createElement('button');
+bt7.id = 'save';
+bt7.innerHTML = 'Save';
+
+
+// buttons.append(bt2);
+// buttons.append(bt3);
+buttons.append(bt4);
+buttons.append(bt5);
+buttons.append(bt6);
+buttons.append(bt7);
+
+fChild.append(buttons);
+
+function moveUp() {
+
+}
+
+function moveDown(){
+
+}
+
+const selectList = listOrder.children;
+
+function eraserSelected() {
+  for (let i in selectList) {
+    const elemento = document.querySelector('.selected');
+    elemento.parentNode.removeChild(elemento);
+  }
+}
+
+bt4.addEventListener('click', eraserSelected);
+
+function eraserMarked() {
+  for (let i in selectList) {
+    const elemento = document.querySelector('.completed');
+    elemento.parentNode.removeChild(elemento);
+  }
+}
+
+bt5.addEventListener('click', eraserMarked);
+
+function saveTask() {
+  localStorage.setItem('task', listOrder.innerHTML);
+}
+
+const clearList = () => listOrder.innerHTML = '';
+
+bt6.addEventListener('click', clearList);
+
+bt7.addEventListener('click', saveTask);
+
+function catchTask() {
+  const task = localStorage.getItem('task');
+  document.querySelector('#lista-tarefas').innerHTML = task;
+}
+catchTask();
+
